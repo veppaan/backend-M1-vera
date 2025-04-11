b7687083fac98e37976f19c3a8a3630de371f59b
Vera Kippel <vera.kippel@outlook.com>
2025-04-11 21:43:17 +0200

    Skapar/installerar databas i install

---
+++ install.js
@@ -0,0 +1,22 @@
+//Installering för SQLite
+
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./db/cv.db");
+
+db.serialize(() =>{
+    db.run("DROP TABLE IF EXISTS courses");
+    db.run(`
+        CREATE TABLE courses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            coursecode VARCHAR(15) NOT NULL,
+            coursename VARCHAR(50) NOT NULL,
+            syllabus VARCHAR (255) NOT NULL,
+            progression VARCHAR(1) NOT NULL,
+            course_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        );
+    `);
+});
+
+
+db.close();
